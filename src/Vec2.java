1e82d2a92809a47fcc88fa5d52de4a948f21c632
Noah Rosamilia <ivoahivoah@gmail.com>
2017-04-05 19:38:02 -0400

    Add Vec2.java

---
+++ src/Vec2.java
@@ -0,0 +1,51 @@
+
+public class Vec2 {
+	private int x;
+	private int y;
+	
+	public Vec2() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Vec2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Vec2(Vec2 copy) {
+		this.x = copy.x;
+		this.y = copy.y;
+	}
+
+	public int getX() { return this.x; }
+	public int getY() { return this.y; }
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Vec2))
+			return false;
+		Vec2 other = (Vec2) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
